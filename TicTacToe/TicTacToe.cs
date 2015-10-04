9c0348dc826bac7532e7a99942ecd40025d1dd42
denyslavangelov <denyslavangelov@5d3144e1-fd40-76d2-400a-ce57b7db7bff>
2015-10-04 10:59:45 +0000

    Adding variables
    git-svn-id: https://github.com/RecommenTeamAdvancedCSharp/Team-Game-2.0/trunk@5 5d3144e1-fd40-76d2-400a-ce57b7db7bff

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -2,8 +2,16 @@
 
 class TicTacToe
 {
+    static char[] position = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    static int playerTurn = 1;
+    static int playerTurnChoise;
+    static int computerChoise;
+    static int winingCondition = 0;
+    static string playerOneName, playerTurnTwoName;
+    static bool isPlayerTurn = true;
+
     static void Main()
     {
-
+        
     }
 }
